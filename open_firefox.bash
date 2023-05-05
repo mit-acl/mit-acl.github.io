78b07f96749f19a7447baeb70425a9be9713022b
Andrew Fishberg <fishberg.dev@gmail.com>
2023-05-05 10:11:44 -0400

    updated open_firefox

--- open_firefox.bash
+++ open_firefox.bash
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 firefox -new-tab http://mit-acl.github.io
+firefox -new-tab https://acl.mit.edu
 firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/actions
+firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/settings/pages
