ecb134e5ea46b5b0670aef6954677b361093d985
Andrew Fishberg <fishberg.dev@gmail.com>
2024-09-16 01:05:56 -0400

    reordered firefox tabs

--- 03_open-firefox.sh
+++ 03_open-firefox.sh
@@ -4,5 +4,5 @@
 #set -x
 
 firefox -new-tab https://acl.mit.edu
-firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/actions
 firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/settings/pages
+firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/actions
