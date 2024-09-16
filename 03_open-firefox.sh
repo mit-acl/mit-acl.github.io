3e529a0926ed821fcb307a4bb6696671e9dad220
Andrew Fishberg <fishberg.dev@gmail.com>
2024-09-16 00:12:38 -0400

    disabled set -x by default

--- 03_open-firefox.sh
+++ 03_open-firefox.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# show commands
+#set -x
+
 firefox -new-tab https://acl.mit.edu
 firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/actions
 firefox -new-tab https://github.com/mit-acl/mit-acl.github.io/settings/pages
